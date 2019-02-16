c41892e8ddc6d571521995e1723505a461143f95
Joseph Roque <contact@josephroque.ca>
2019-02-16 10:53:08 -0500

    Player tests

---
+++ Tests/HiveEngineTests/State/PlayerTests.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerTests.swift
+//  HiveEngineTests
+//
+//  Created by Joseph Roque on 2019-02-14.
+//  Copyright © 2019 Joseph Roque. All rights reserved.
+//
+
+import XCTest
+@testable import HiveEngine
+
+final class PlayerTests: HiveEngineTestCase {
+
+	static var allTests = [
+		("testPlayerNextPlayer", testPlayerNextPlayer)
+	]
+
+	func testPlayerNextPlayer() {
+		XCTAssertEqual(Player.black, Player.white.next)
+		XCTAssertEqual(Player.white, Player.black.next)
+	}
+}
