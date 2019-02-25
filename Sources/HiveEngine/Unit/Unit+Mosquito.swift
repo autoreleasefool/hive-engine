c2f8d28b7c702378310cdbf8af336076715c3dc5
Joseph Roque <contact@josephroque.ca>
2019-02-24 20:39:31 -0500

    Ensure mosquito can move as beetles when on top of hive

--- Sources/HiveEngine/Unit/Unit+Mosquito.swift
+++ Sources/HiveEngine/Unit/Unit+Mosquito.swift
@@ -12,12 +12,17 @@ extension Unit {
 	func movesAsMosquito(in state: GameState) -> Set<Movement> {
 		guard self.canMove(in: state) else { return [] }
 		guard self.canMove(as: .mosquito, in: state) else { return [] }
+		guard let position = state.units[self], position != .inHand, let height = self.stackPosition(in: state) else { return [] }
 		var moves = Set<Movement>()
 
-		// Copy moves of each adjacent unit
-		state.units(adjacentTo: self).forEach {
-			guard $0.class != .mosquito else { return }
-			moves = moves.union(self.moves(as: $0.class, in: state))
+		if height > 1 {
+			moves = self.movesAsBeetle(in: state)
+		} else {
+			// Copy moves of each adjacent unit
+			state.units(adjacentTo: self).forEach {
+				guard $0.class != .mosquito else { return }
+				moves = moves.union(self.moves(as: $0.class, in: state))
+			}
 		}
 
 		return moves
