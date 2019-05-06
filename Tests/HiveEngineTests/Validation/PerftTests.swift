ce372a946c1b6c9769d52f3bcce2b680adaa97d0
Joseph Roque <contact@josephroque.ca>
2019-05-06 09:13:33 -0400

    Perft test formatting, add additional values to check

--- Tests/HiveEngineTests/Validation/PerftTests.swift
+++ Tests/HiveEngineTests/Validation/PerftTests.swift
@@ -25,14 +25,14 @@ class PerftTests: HiveEngineTestCase {
 	/// 2. If a player has multiple bugs of the same type in their hand, then only one is used to calculate the number of valid placements. Ie. if you have two ants in your hand, and five open positions to place a bug, you have five valid moves, not ten.
 	/// 3. A player can only Pass if there are no other legal moves, and then that Pass counts as one move for the purposes of calculating perft.
 	private let perftTable: [String: [Int]] = [
-		"": [4, 96, 1440, 21600, 516240, 12219480],
-		"M": [5, 150, 2610, 45414, 1252800, 34233432],
-		"L": [5, 150, 2610, 45414, 1252800, 34233672],
-		"P": [5, 150, 2610, 45414, 1255932, 34395984],
-		"ML": [6, 216, 4320, 86400, 2725920, 85201200],
-		"MP": [6, 216, 4320, 86400, 2730888, 85492248],
-		"LP": [6, 216, 4320, 86400, 2730240, 85457136],
-		"MLP": [7, 294, 6678, 151686, 5427108, 192353904]
+		"": [4, 96, 1_440, 21_600, 516_240, 12_219_480, 181_641_900, 2_657_392_800],
+		"M": [5, 150, 2_610, 45_414, 1_252_800, 34_233_432],
+		"L": [5, 150, 2_610, 45_414, 1_252_800, 34_233_672],
+		"P": [5, 150, 2_610, 45_414, 1_255_932, 34_395_984],
+		"ML": [6, 216, 4_320, 86_400, 2_725_920, 85_201_200],
+		"MP": [6, 216, 4_320, 86_400, 2_730_888, 85_492_248],
+		"LP": [6, 216, 4_320, 86_400, 2_730_240, 85_457_136],
+		"MLP": [7, 294, 6_678, 151_686, 5_427_108, 192_353_904]
 	]
 
 	/// Count the number of valid states at a certain depth by iterating all possible moves.
