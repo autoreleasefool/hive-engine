a2a60af12b549ed80b09be8ddbd8d0a9d4f838ff
Joseph Roque <contact@josephroque.ca>
2020-01-13 00:16:09 +0100

    New game command tests

--- Tests/HiveEngineTests/Engine/Commands/UHPCommandTests.swift
+++ Tests/HiveEngineTests/Engine/Commands/UHPCommandTests.swift
@@ -11,10 +11,29 @@ import XCTest
 
 final class UHPCommandTests: HiveEngineTestCase {
 	func testInfoCommand() {
-		var state: GameState?
-
-		let result = InfoCommand().invoke("", state: &state)
+		let result = InfoCommand().invoke("", state: nil)
 		XCTAssertEqual(.output("id Hive Engine v2.0.0\nMosquito;Ladybug;Pillbug"), result)
+	}
+
+	func testNewGameCommand_Base() {
+		let result = NewGameCommand().invoke("Base", state: nil)
+		XCTAssertEqual(.state(GameState()), result)
+	}
+
+	func testNewGameCommand_BaseMLP() {
+		let result = NewGameCommand().invoke("Base+MLP", state: nil)
+		XCTAssertEqual(.state(GameState(options: [.mosquito, .ladyBug, .pillBug])), result)
+	}
+
+	func testNewGameCommand_GameString() {
+		let state = GameState()
+		state.apply(.place(unit: state.whiteSpider, at: .origin))
+		state.apply(.place(unit: state.blackHopper, at: Position(x: -1, y: 1, z: 0)))
+		state.apply(.place(unit: state.whiteAnt, at: Position(x: 1, y: 0, z: -1)))
+		state.apply(.place(unit: state.blackSpider, at: Position(x: -2, y: 1, z: 1)))
+
+		let result = NewGameCommand().invoke("Base;InProgress;White[3];wS1;bG1 -wS1;wA1 wS1/;bS1 /bG1", state: nil)
+		XCTAssertEqual(.state(state), result)
 	}
 
 	static var allTests = [
