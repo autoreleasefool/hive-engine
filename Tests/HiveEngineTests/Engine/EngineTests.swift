6c0bfd849124e30167a24185c009e5e5bfdbe8b4
Joseph Roque <github@josephroque.ca>
2020-08-17 08:25:31 -0400

    Engine tests

---
+++ Tests/HiveEngineTests/Engine/EngineTests.swift
@@ -0,0 +1,47 @@
+//
+//  EngineTests.swift
+//  HiveEngineTests
+//
+//  Created by Joseph Roque on 2020-08-14.
+//
+
+import Regex
+import XCTest
+@testable import HiveEngine
+
+final class EngineTests: HiveEngineTestCase {
+
+	func testEngineHasNoInitialState() {
+		let engine = Engine()
+		XCTAssertNil(engine.state)
+	}
+
+	func testEngineIgnoresInvalidCommand() {
+		let engine = Engine()
+		XCTAssertEqual(nil, engine.send(input: ""))
+		XCTAssertEqual(nil, engine.send(input: "INVALID"))
+	}
+
+	func testEngineParsesCommand() {
+		let engine = Engine()
+
+		guard case let .output(message) = engine.send(input: "info") else {
+			XCTFail("Command result was not valid")
+			return
+		}
+
+		XCTAssertTrue(message.starts(with: "id"))
+	}
+
+	func testEngineParsesCommandAndInput() {
+		let engine = Engine()
+		XCTAssertEqual(.state(GameState()), engine.send(input: "newgame Base;NotStarted;White[1]"))
+	}
+
+	static var allTests = [
+		("testEngineHasNoInitialState", testEngineHasNoInitialState),
+		("testEngineIgnoresInvalidCommand", testEngineIgnoresInvalidCommand),
+		("testEngineParsesCommand", testEngineParsesCommand),
+		("testEngineParsesCommandAndInput", testEngineParsesCommandAndInput),
+	]
+}
